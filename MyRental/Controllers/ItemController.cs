775a6643b5fc4d91e7821aacafdc8891c26ad866
louis297 <xl297@georgetown.edu>
2020-01-05 12:59:55 +1300

    [init]item list

---
+++ MyRental/Controllers/ItemController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using myrental.Models;
+using Rental.DTOs.ItemDTOs;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyRental.Controllers
+{
+    [Route("api/[controller]")]
+    public class ItemController : Controller
+    {
+
+        private MyRentalDbContext context;
+
+        public ItemController()
+        {
+            context = new MyRentalDbContext();
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<ItemListDTO> Get()
+        {
+            var items = context.items.Select(item => new ItemListDTO()
+            {
+                Title = item.Title,
+                Detail = item.Detail,
+                Price = item.Price,
+                PostTime = item.PostTime,
+                ExpireTime = item.ExpireTime
+                //TODO: add AuthorName field
+                //AuthorName = item.Author.userName
+            });
+            return items;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ItemListDTO Get(int id)
+        {
+            var items = context.items.Where(i => i.ItemID == id)
+                .Select(item => new ItemListDTO()
+                {
+                    Title = item.Title,
+                    Detail = item.Detail,
+                    Price = item.Price,
+                    PostTime = item.PostTime,
+                    ExpireTime = item.ExpireTime
+                    //TODO: add AuthorName field
+                    //AuthorName = item.Author.userName
+                });
+            return items.Count() > 0 ? items.First() : null;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MyRental/Migrations/20200104232837_[init]Item.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyRental.Migrations
+{
+    public partial class initItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "items",
+                columns: table => new
+                {
+                    ItemID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(maxLength: 255, nullable: false),
+                    Detail = table.Column<string>(maxLength: 1000, nullable: false),
+                    PostTime = table.Column<DateTime>(nullable: false, defaultValueSql: "CURRENT_TIMESTAMP(6)"),
+                    ExpireTime = table.Column<DateTime>(nullable: false),
+                    Price = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_items", x => x.ItemID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "items",
+                columns: new[] { "ItemID", "Detail", "ExpireTime", "PostTime", "Price", "Title" },
+                values: new object[] { 1, "details1", new DateTime(2020, 6, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 200, "item1" });
+
+            migrationBuilder.InsertData(
+                table: "items",
+                columns: new[] { "ItemID", "Detail", "ExpireTime", "PostTime", "Price", "Title" },
+                values: new object[] { 2, "details2", new DateTime(2020, 7, 1, 6, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 100, "item2" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "items");
+        }
+    }
+}
