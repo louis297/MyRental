4884d6068888f37c9ee48e28030fe2e4966b1b2f
louis297 <xl297@georgetown.edu>
2020-01-07 14:34:06 +1300

    change start url to /api/item

--- MyRental/Services/ItemServices/IItemService.cs
+++ MyRental/Services/ItemServices/IItemService.cs
@@ -8,5 +8,7 @@ namespace MyRental.Services.ItemServices
     {
         public IEnumerable<ItemListDTO> GetItemList();
         public ItemListDTO GetItemDetail(int id);
+        public ItemListDTO Get(int id);
+        public void Post(string value);
     }
 }
