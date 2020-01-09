7b9a194e4324804920355d713c45858e97850408
louis297 <xl297@georgetown.edu>
2020-01-09 16:57:54 +1300

    add unit test project

---
+++ MyRentalTest/ControllerTest/ItemControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text.Json;
+using MyRental.DTOs.ItemDTOs;
+using MyRental.Services.ItemServices;
+using MyRental.Tests.ControllerTest;
+using Xunit;
+
+namespace MyRentalTest.ControllerTest
+{
+    public class ItemControllerTest
+    {
+        private IItemService service;
+
+
+        public ItemControllerTest()
+        {
+            service = new ItemServiceMock();
+            Console.WriteLine("aasfawefawefawefwfawef");
+        }
+        [Fact]
+        public void GetListTest()
+        {
+            var r = service.GetItemList();
+            var i = r.GetEnumerator();
+
+            Assert.Equal(@"[{""ItemName"":""item1"",""Detail"":""details1"",""PostTime"":""0001-01-01T00:00:00"",""ExpireTime"":""2020-06-01T00:00:00"",""Price"":200},{""ItemName"":""item2"",""Detail"":""details2"",""PostTime"":""0001-01-01T00:00:00"",""ExpireTime"":""2020-07-01T06:00:00"",""Price"":100}]", JsonSerializer.Serialize(r));
+
+        }
+
+        [Fact]
+        public void GetDetailTest()
+        {
+            var r = service.GetItemDetailById(1);
+            Assert.Equal(@"{""ItemName"":""item1"",""Detail"":""details1"",""PostTime"":""0001-01-01T00:00:00"",""ExpireTime"":""2020-06-01T00:00:00"",""Price"":200}", JsonSerializer.Serialize(r));
+        }
+
+        [Fact]
+        public void CreateItemTest()
+        {
+            var m = new ItemCreateDTO
+            {
+                ItemName = "aa",
+                Detail = "bb",
+                Price = 30,
+                ExpireTime = DateTime.Parse("2030-06-01T00:00:00")
+            };
+            var r = service.CreateItem(m);
+            Assert.Equal("{'result': 'success'}", r);
+
+            m.ItemName = "aaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdfaaaaaaaaasdfasdfasdasdfasdf";
+            r = service.CreateItem(m);
+            Assert.Equal("{'result': 'failed', 'reason': 'Item name is too long'}", r);
+
+        }
+    }
+}
---
+++ MyRentalTest/ServicesTest/ItemServiceMock.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using MyRental.DTOs.ItemDTOs;
+using MyRental.Models.ItemModels;
+using MyRental.Services.ItemServices;
+
+namespace MyRental.Tests.ControllerTest
+{
+    public class ItemServiceMock: IItemService
+    {
+        public ItemServiceMock()
+        {
+        }
+
+        public string CreateItem(ItemCreateDTO newItem)
+        {
+            try
+            {
+                if (newItem.ItemName.Length > 250)
+                {
+                    return "{'result': 'failed', 'reason': 'Item name is too long'}";
+                }
+                if (newItem.Detail.Length > 999)
+                {
+                    return "{'result': 'failed', 'reason': 'Item detail is too long'}";
+                }
+                if (newItem.Price < 0)
+                {
+                    return "{'result': 'failed', 'reason': 'Price should not be negative'}";
+                }
+                if (newItem.ExpireTime < DateTime.Now)
+                {
+                    return "{'result': 'failed', 'reason': 'Expire time should not be in the past'}";
+                }
+
+                return "{'result': 'success'}";
+            } catch
+            {
+                return "{'result': 'failed'}";
+            }
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItemById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ItemListDTO GetItemDetailById(int id)
+        {
+            if (id == 1)
+            {
+                var item = new ItemListDTO
+                {
+                    ItemName = "item1",
+                    Detail = "details1",
+                    ExpireTime = DateTime.Parse("2020-06-01 00:00:00"),
+                    Price = 200
+                };
+                return item;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+
+        public IEnumerable<ItemListDTO> GetItemList()
+        {
+            var items = new List<ItemListDTO>();
+            items.Add(new ItemListDTO
+            {
+                ItemName = "item1",
+                Detail = "details1",
+                ExpireTime = DateTime.Parse("2020-06-01 00:00:00"),
+                Price = 200
+            });
+            items.Add(new ItemListDTO
+            {
+                ItemName = "item2",
+                Detail = "details2",
+                ExpireTime = DateTime.Parse("2020-07-01 06:00:00"),
+                Price = 100
+            });
+            return items;
+        }
+    }
+}
